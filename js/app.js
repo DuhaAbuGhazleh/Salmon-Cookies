7e655ca1e2bbe7fccb079eae78f3e12f886f1bfa
DuhaAbuGhazleh <duhamahmoud0@gmail.com>
2021-08-02 23:54:45 +0300

    add construcror function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
-
-const hours = ['       ', '6am' , '7am' ,'8pm' , '9am' , '10am' , '11am ','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm',' daily total'];
+let finalColumnArray =[0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+const hours = [' Region      ', '6am' , '7am' ,'8pm' , '9am' , '10am' , '11am ','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm',' daily total'];
 
 let cookiesInfo = document.getElementById('info');
 
@@ -8,7 +8,9 @@ let articleElement = document.createElement('article');
 cookiesInfo.appendChild(articleElement);
 let tableelement = document.createElement('table');
 articleElement.appendChild(tableelement);
+tableelement.setAttribute("border","2");
 let trHead=document.createElement('tr');
+
 tableelement.appendChild(trHead);
 
 for(let i=0 ; i<hours.length; i++){
@@ -46,21 +48,26 @@ SalmonCookies.prototype.gitCookies= function() {
 };
 
 
+
 SalmonCookies.prototype.render= function(){
+  let totalrow=0;
   let trRow = document.createElement('tr');
   tableelement.appendChild(trRow);
 
   let column1 = document.createElement('th');
   column1.textContent = this.name;
   trRow.appendChild(column1);
-  for(let i=1 ; i <hours.length ;i++){
+  for(let i=0 ; i <14 ;i++){
     let contant = document.createElement('td');
     contant.textContent=this.cookiesperh[i];
     trRow.appendChild(contant);
+    totalrow+= this.cookiesperh[i];
+    finalColumnArray[i]+= this.cookiesperh[i];
+    console.log(totalrow);
 
   }
   let columnTotal = document.createElement('td');
-  columnTotal.textContent=this.total;
+  columnTotal.textContent=totalrow;
   trRow.appendChild(columnTotal);
 
 };
@@ -85,11 +92,11 @@ for (let n=0; n<city.length ; n++){
 function footer (){
   let trRow =document.createElement('tr');
   tableelement.appendChild(trRow);
-  let column1 = document.createElement('td');
-  column1.textContent='total';
+  let column1 = document.createElement('th');
+  column1.textContent='Total';
   trRow.appendChild(column1);
 
-  let total2 = [0];
+  //let total2 = 0;
   let allTotal=0;
   for (let j=0; j<city.length; j++){
 
@@ -97,20 +104,16 @@ function footer (){
 
 
   }
-  for( let i =0 ; i< hours.length ; i++){
+  for( let i =0 ; i< finalColumnArray.length ; i++){
 
 
-    for (let j=0 ; j< city.length ; j++){
-      total2 += city[j].sales[i];
-
-
-    }
-    let td = document.createElement('td');
-    td.textContent = total2;
+   
+    let td = document.createElement('th');
+    td.textContent = finalColumnArray[i];
     trRow.appendChild(td);
 
   }
-  let tdt = document.createElement('td');
+  let tdt = document.createElement('th');
   tdt.textContent = allTotal;
   trRow.appendChild(tdt);
 }
